f90e9a6e649add2d8d86b29258dc81f45e3b981e
An Phi <aphi@skidmore.edu>
2017-11-16 10:35:22 -0500

    Modify script to treat passenger class as categorical variable

--- data/script.R
+++ data/script.R
@@ -1,10 +1,17 @@
 library(dplyr)
-titanic <- read_csv("<directory>/aStudyInDisaster/data/titanic3.csv")
+DIR <- "<directory>"
+
+# Import
+titanic <- read.csv(paste(DIR, "aStudyInDisaster/data/titanic3.csv", sep = "/"))
 titanic <- titanic %>% select(-boat, -name, -home.dest, -cabin, -body, -ticket)
 summary(titanic)
+
+# Remove uneccesary variables
 for (name in c("pclass", "survived", "sex", "sibsp", "parch", "fare", "embarked")) {
   titanic <- titanic[c(!is.na(titanic[name])),]
 }
+
+# Process gender
 titanic$isFemale = as.integer(titanic$sex == "female")
 titanic$sex = NULL
 
@@ -28,6 +35,15 @@ titanic$fare <- as.numeric(format(round(titanic$fare, 2), nsmall = 2))
 
 # Rename columns
 colnames(titanic) <- c("passenger_class", "has_survived", "number_of_siblings_and_spouses", "number_of_parents_and_children", "fare", "is_female", "embarked_from_cherbourg", "embarked_from_queenstown", "embarked_from_southampton", "age_missing", "age_between_0_and_5", "age_between_6_and_18", "age_between_19_and_55", "age_between_56_and_above")
+
+# Process passenger class
+titanic$passenger_class <- as.factor(titanic$passenger_class)
+
+# Add ID column
 titanic$passenger_id <- as.integer(row.names(titanic))
+
+# Rearragne columns
 titanic <- titanic[,c(15,2,6,10,11,12,13,14,3,4,1,5,7,8,9)]
-write_csv(titanic, "<directory>/aStudyInDisaster/data/titanic.csv", na="NA")
+
+# Export
+write.csv(titanic, paste(DIR, "aStudyInDisaster/data/titanic.csv", sep = "/"), na="NA")
